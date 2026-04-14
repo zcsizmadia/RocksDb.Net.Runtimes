64206ecb9fb4ea53a37ae607c4a845e8bf6abd0d
Zoltan Csizmadia <zcsizmadia@gmail.com>
2026-04-13 22:02:37 -0500

    Debug

--- vcpkg-overlays/triplets/linux-musl-x64.cmake
+++ vcpkg-overlays/triplets/linux-musl-x64.cmake
@@ -4,9 +4,3 @@ set(VCPKG_LIBRARY_LINKAGE dynamic)
 set(VCPKG_CMAKE_SYSTEM_NAME Linux)
 set(VCPKG_FIXUP_ELF_RPATH ON)
 set(VCPKG_BUILD_TYPE release)
-
-# This replaces the need for patchelf
-set(VCPKG_LINKER_FLAGS "-Wl,-rpath,'$ORIGIN' -Wl,-z,origin")
-
-# Ensure vcpkg doesn't try to "fix" (remove) the RPATH we just set
-set(VCPKG_FIXUP_ELF_RPATH OFF)
